b1c246b98ea4c03faf3ab2d89baaf44827620c10
kikiya <kikia@cockroachlabs.com>
2022-03-03 13:21:45 -0500

    copy code to all exercises

--- exercises/00-initial-state/movr/cockroach/run.sh
+++ exercises/00-initial-state/movr/cockroach/run.sh
@@ -2,4 +2,5 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-cockroach demo --sql-port 26257 --no-example-database
+# cockroach demo --sql-port 26257 --no-example-database
+cockroach demo --global --sql-port 26257 --no-example-database --nodes 9
