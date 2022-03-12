d03fd39ae4518aa7d567150ed4912b7596ee6f05
kikiya <kikia@cockroachlabs.com>
2022-03-12 12:53:22 -0500

    make localities more generic

--- exercises/02-poor-resilience/movr/cockroach/run.sh
+++ exercises/02-poor-resilience/movr/cockroach/run.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 IFS=$'\n\t'
 
 # start demo db with localities using  generic region names not gcp or aws specific 
-cockroach demo --sql-port 26257 --no-example-database --nodes=9 --demo-locality=region=us-east1,zone=a:region=us-east1,zone=b:region=us-east2,zone=c:region=us-west1,zone=b:region=us-west1,zone=c:region=us-west1,zone=d:region=europe-west1,zone=a:region=europe-west1,zone=b:region=europe-west1,zone=c --empty
+cockroach demo --insecure --sql-port 26257 --no-example-database --nodes=9 --demo-locality=region=us-east,zone=a:region=us-east,zone=b:region=us-east,zone=c:region=us-west,zone=b:region=us-west,zone=c:region=us-west,zone=d:region=us-central,zone=a:region=us-central,zone=b:region=us-central,zone=c --empty
