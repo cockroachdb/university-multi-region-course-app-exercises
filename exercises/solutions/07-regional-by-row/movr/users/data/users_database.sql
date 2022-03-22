887ca77104f77ddacb66f031adc778bd07c0e7f6
kikiya <kikia@cockroachlabs.com>
2022-03-22 15:37:29 -0400

    Regional by row (#8)
    * update user table to include city, add exercise solution folder
    
    * update rbr solution for 07 and move under movr folder

---
+++ exercises/solutions/07-regional-by-row/movr/users/data/users_database.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE movr_users;
+
+CREATE TABLE movr_users.users (
+	email STRING PRIMARY KEY,
+	city STRING NOT NULL,
+	last_name STRING NOT NULL,
+	first_name STRING NOT NULL,
+	phone_numbers STRING[] NOT NULL
+);
+
+ALTER DATABASE movr_users SET PRIMARY REGION "us-east";
+ALTER DATABASE movr_users ADD REGION "us-central";
+ALTER DATABASE movr_users ADD REGION "us-west";
+
+ALTER TABLE movr_users.users SET LOCALITY REGIONAL BY ROW;
