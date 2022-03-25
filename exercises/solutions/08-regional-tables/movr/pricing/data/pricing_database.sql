7b13a7bd7e633dce7353c0988e38b5072911afea
kikiya <kikia@cockroachlabs.com>
2022-03-25 12:13:05 -0400

    make solutions from previous exercise available in current solution folder, ex 06 sol folder

--- exercises/solutions/08-regional-tables/movr/pricing/data/pricing_database.sql
+++ exercises/solutions/08-regional-tables/movr/pricing/data/pricing_database.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE movr_pricing;
 
-ALTER DATABASE movr_pricing SET PRIMARY REGION "us-east";
+ALTER DATABASE movr_pricing SET PRIMARY REGION "us-west";
 ALTER DATABASE movr_pricing ADD REGION "us-central";
-ALTER DATABASE movr_pricing ADD REGION "us-west";
+ALTER DATABASE movr_pricing ADD REGION "us-east";
 
 CREATE TABLE movr_pricing.promo_codes (
     code STRING PRIMARY KEY,
@@ -10,6 +10,13 @@ CREATE TABLE movr_pricing.promo_codes (
     creation_time TIMESTAMP NOT NULL, 
     expiration_time TIMESTAMP NOT NULL, 
     rules JSONB NOT NULL
+);
+
+CREATE TABLE movr_pricing.vip_rates (
+    rate_code STRING PRIMARY KEY,
+    market STRING NOT NULL,
+    description STRING NOT NULL, 
+    creation_time TIMESTAMP NOT NULL, 
 );
 
 ALTER TABLE movr_pricing.promo_codes SET locality GLOBAL;
