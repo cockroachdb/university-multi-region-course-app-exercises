2c533c3aa333990c0421f512574fb51c9640a529
kikiya <kikia@cockroachlabs.com>
2022-03-29 13:44:19 -0400

    Update solution exercise scripts (#14)
    * update scripts and course code structure per PR comments
    
    * using the original scripts with minimal configuration changes
    
    * remove build.sh as not relevant per PR

---
+++ solutions/01-identifying-necessary-regions/movr/cockroach/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+# start demo db with localities using  generic region names not gcp or aws specific 
+cockroach demo --insecure --sql-port 26257 --no-example-database --nodes=9 --demo-locality=region=us-east,zone=a:region=us-east,zone=b:region=us-east,zone=c:region=us-west,zone=b:region=us-west,zone=c:region=us-west,zone=d:region=us-central,zone=a:region=us-central,zone=b:region=us-central,zone=c:region=eu-central,region=sa-east --empty
