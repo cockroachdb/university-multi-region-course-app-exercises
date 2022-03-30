c987ea9b7e16f7fdee33ef301ad8178155a0dfcf
kikiya <kikia@cockroachlabs.com>
2022-03-30 11:55:45 -0400

    allow null city (#17)

--- solutions/08-placement-restricted/users/data/users_database.sql
+++ solutions/08-placement-restricted/users/data/users_database.sql
@@ -2,7 +2,7 @@ CREATE DATABASE movr_users;
 
 CREATE TABLE movr_users.users (
 	email STRING PRIMARY KEY,
-	city STRING NOT NULL,
+	city STRING NULL,
 	last_name STRING NOT NULL,
 	first_name STRING NOT NULL,
 	phone_numbers STRING[] NOT NULL
