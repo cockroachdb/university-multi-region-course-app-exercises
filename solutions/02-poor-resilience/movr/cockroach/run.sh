84599810691083aedde2fe1892746cc47ac09ff6
kikiya <kikia@cockroachlabs.com>
2022-04-01 11:56:05 -0400

    Test fixes (#19)
    * various fixes for issues found during uat

--- solutions/02-poor-resilience/movr/cockroach/run.sh
+++ solutions/02-poor-resilience/movr/cockroach/run.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 IFS=$'\n\t'
 
 # start demo db with localities using  generic region names not gcp or aws specific 
-cockroach demo --insecure --sql-port 26257 --no-example-database --nodes=9 --demo-locality=region=us-east,zone=a:region=us-east,zone=b:region=us-east,zone=c:region=us-west,zone=b:region=us-west,zone=c:region=us-west,zone=d:region=us-central,zone=a:region=us-central,zone=b:region=us-central,zone=c --empty
+cockroach demo --sql-port 26257 --no-example-database --nodes=9 --demo-locality=region=us-east,zone=a:region=us-east,zone=b:region=us-east,zone=c:region=us-west,zone=b:region=us-west,zone=c:region=us-west,zone=d:region=us-central,zone=a:region=us-central,zone=b:region=us-central,zone=c --empty
