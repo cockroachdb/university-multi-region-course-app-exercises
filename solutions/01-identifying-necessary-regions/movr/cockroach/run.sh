2c1983454568b5c8ea330735050f5d5810d3d531
kikiya <kikia@cockroachlabs.com>
2022-04-01 11:09:13 -0400

    Exercise fixes (#18)

--- solutions/01-identifying-necessary-regions/movr/cockroach/run.sh
+++ solutions/01-identifying-necessary-regions/movr/cockroach/run.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 IFS=$'\n\t'
 
 # start demo db with localities using  generic region names not gcp or aws specific 
-cockroach demo --insecure --sql-port 26257 --no-example-database --nodes=9 --demo-locality=region=us-east,zone=a:region=us-east,zone=b:region=us-east,zone=c:region=us-west,zone=b:region=us-west,zone=c:region=us-west,zone=d:region=us-central,zone=a:region=us-central,zone=b:region=us-central,zone=c:region=eu-central,region=sa-east --empty
+cockroach demo --insecure --sql-port 26257 --no-example-database --nodes=9 --demo-locality=region=us-east,zone=a:region=us-east,zone=b:region=us-east,zone=c:region=us-west,zone=b:region=us-west,zone=c:region=us-west,zone=d:region=us-central,zone=a:region=sa-east,zone=b:region=eu-central,zone=c --empty
