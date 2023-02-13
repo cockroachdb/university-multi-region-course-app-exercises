b0b8c0f0a33e19547061ec949534ac33736765ad
kikiya <kikia@cockroachlabs.com>
2023-02-13 10:06:16 -0500

    Bug fix ex 6 8 (#31)
    * update lesson 8&9 setup and solution to clarify rbr where missing
    
    * add user data to enhance exercise

--- setup/09-tradeoffs/movr/users/data/users_database.sql
+++ setup/09-tradeoffs/movr/users/data/users_database.sql
@@ -13,4 +13,19 @@ ALTER DATABASE movr_users ADD REGION "us-central";
 ALTER DATABASE movr_users ADD REGION "us-west";
 ALTER DATABASE movr_users ADD REGION "eu-west";
 
+ALTER TABLE movr_users.users SET LOCALITY REGIONAL BY ROW;
+
 ALTER DATABASE movr_users PLACEMENT RESTRICTED;
+
+INSERT INTO movr_users.users (email,city,last_name,first_name,phone_numbers,crdb_region)
+VALUES
+  ('in.felis.nulla@protonmail.edu','Zaria','Daugherty','Tamara', ARRAY['505-505-5050'],'us-east'),
+  ('etiam.laoreet@aol.couk','San Rafael','Tucker','Kareem',ARRAY['505-505-5051'],'us-west'),
+  ('nullam.ut@yahoo.com','Bremerhaven','Nolan','Seth',ARRAY['505-505-5052'],'us-east'),
+  ('elit.pellentesque@aol.couk','Ciplet','Daniels','Carolyn',ARRAY['505-505-5053'],'us-west'),
+  ('ut.eros@outlook.couk','Torrevieja','Morton','Simon',ARRAY['505-505-5054'],'us-east'), 
+  ('neque.tellus@google.com','Rastatt','Howard','Penelope',ARRAY['505-505-5055'],'us-west'),
+  ('mi@aol.com','Roodepoort','Jacobson','Mason',ARRAY['505-505-5056'],'us-central'),
+  ('morbi.accumsan@yahoo.com','Landeck','Harper','Theodore',ARRAY['505-505-5057'],'us-west'),
+  ('elit.a.feugiat@protonmail.net','Uruapan','Shelton','Violet',ARRAY['505-505-5058'],'us-central'),
+  ('magna.a.neque@google.net','Adana','Jensen','Hayes',ARRAY['505-505-5059'],'us-central');
