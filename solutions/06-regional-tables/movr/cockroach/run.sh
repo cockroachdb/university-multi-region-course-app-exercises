42ebc6dcda9ea29087801b720db0d4e2726284b2
kikiya <kikia@cockroachlabs.com>
2023-03-01 15:07:54 -0500

    kill demo before running in case it's running

--- solutions/06-regional-tables/movr/cockroach/run.sh
+++ solutions/06-regional-tables/movr/cockroach/run.sh
@@ -2,8 +2,6 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-#!/bin/bash
-set -euo pipefail
-IFS=$'\n\t'
+pkill -term -f 'cockroach demo'
 
 cockroach demo --sql-port 26257 --no-example-database --nodes=9 --demo-locality=region=us-east,zone=a:region=us-east,zone=b:region=us-east,zone=c:region=us-west,zone=b:region=us-west,zone=c:region=us-west,zone=d:region=us-central,zone=a:region=us-central,zone=d:region=us-central,zone=f --empty
--- solutions/07-placement-restricted/movr/cockroach/run.sh
+++ solutions/07-placement-restricted/movr/cockroach/run.sh
@@ -2,4 +2,6 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+pkill -term -f 'cockroach demo'
+
 cockroach demo --sql-port 26257 --no-example-database --nodes=12 --demo-locality=region=us-east,zone=a:region=us-east,zone=b:region=us-east,zone=c:region=us-west,zone=b:region=us-west,zone=c:region=us-west,zone=d:region=us-central,zone=a:region=us-central,zone=d:region=us-central,zone=f:region=eu-west,zone=a:region=eu-west,zone=d:region=eu-west,zone=f --empty
