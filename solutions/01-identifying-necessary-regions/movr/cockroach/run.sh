64e812463054f8f7bd649c9141e039308d4e2991
kikiya <kikia@cockroachlabs.com>
2023-03-01 16:25:37 -0500

    kill demo before running in case it's running (#35)

--- solutions/01-identifying-necessary-regions/movr/cockroach/run.sh
+++ solutions/01-identifying-necessary-regions/movr/cockroach/run.sh
@@ -2,5 +2,7 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+pkill -term -f 'cockroach demo'
+
 # start demo db with localities using  generic region names not gcp or aws specific 
 cockroach demo --sql-port 26257 --no-example-database --nodes=9 --demo-locality=region=us-east,zone=a:region=us-east,zone=b:region=us-east,zone=c:region=us-west,zone=b:region=us-west,zone=c:region=us-west,zone=d:region=us-central,zone=a:region=sa-east,zone=b:region=eu-central,zone=c --empty
