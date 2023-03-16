5c3a6f521fff6446791e8e21664c208951f6c5f0
kikiya <kikia@cockroachlabs.com>
2023-03-16 09:57:33 -0400

    should start with init state

--- exercises/movr/cockroach/run.sh
+++ exercises/movr/cockroach/run.sh
@@ -4,5 +4,4 @@ IFS=$'\n\t'
 
 
 
-# start demo db with localities using  generic region names not gcp or aws specific 
-cockroach demo --sql-port 26257 --no-example-database --nodes=9 --demo-locality=region=us-east,zone=a:region=us-east,zone=b:region=us-east,zone=c:region=us-west,zone=b:region=us-west,zone=c:region=us-west,zone=d:region=us-central,zone=a:region=sa-east,zone=b:region=eu-central,zone=c --empty
+cockroach demo --sql-port 26257 --nodes=9 --no-example-database
