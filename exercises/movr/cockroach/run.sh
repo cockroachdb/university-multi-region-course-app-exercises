8129be7914d32376c1795d7abcbbb9cf8797f5a9
kikiya <kikia@cockroachlabs.com>
2023-03-16 09:47:46 -0400

    remove pkill from run

--- exercises/movr/cockroach/run.sh
+++ exercises/movr/cockroach/run.sh
@@ -2,4 +2,7 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-cockroach demo --sql-port 26257 --nodes=9 --no-example-database
+
+
+# start demo db with localities using  generic region names not gcp or aws specific 
+cockroach demo --sql-port 26257 --no-example-database --nodes=9 --demo-locality=region=us-east,zone=a:region=us-east,zone=b:region=us-east,zone=c:region=us-west,zone=b:region=us-west,zone=c:region=us-west,zone=d:region=us-central,zone=a:region=sa-east,zone=b:region=eu-central,zone=c --empty
