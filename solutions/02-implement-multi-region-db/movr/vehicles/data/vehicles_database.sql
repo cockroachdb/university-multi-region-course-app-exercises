114514a7a3abb5636b69513ded6ae14953ff7191
kikiya <kikia@cockroachlabs.com>
2023-05-05 12:54:35 -0400

    updated users to add pkey constraint, use auto generated uuid on all tables

--- solutions/02-implement-multi-region-db/movr/vehicles/data/vehicles_database.sql
+++ solutions/02-implement-multi-region-db/movr/vehicles/data/vehicles_database.sql
@@ -1,8 +1,7 @@
 CREATE DATABASE movr_vehicles;
 
 CREATE TABLE movr_vehicles.vehicles (
-    id UUID PRIMARY KEY,
-    battery INT8 NULL,
+    id UUID NOT NULL DEFAULT gen_random_uuid(),battery INT8 NULL,
     last_ride_start TIMESTAMP NULL,
     last_ride_end TIMESTAMP NULL,
     vehicle_info JSON NULL,
@@ -12,8 +11,7 @@ CREATE TABLE movr_vehicles.vehicles (
 );
 
 CREATE TABLE movr_vehicles.location_history(
-    id UUID PRIMARY KEY,
-    vehicle_id UUID REFERENCES movr_vehicles.vehicles(id) ON DELETE CASCADE,
+    id UUID NOT NULL DEFAULT gen_random_uuid(),vehicle_id UUID REFERENCES movr_vehicles.vehicles(id) ON DELETE CASCADE,
     ts TIMESTAMP NOT NULL,
     longitude FLOAT8 NOT NULL,
     latitude FLOAT8 NOT NULL
