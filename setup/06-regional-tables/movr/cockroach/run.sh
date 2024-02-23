fc0554a3c7848897389be1892bd1c9c1df3e22d7
kikiya <kikia@cockroachlabs.com>
2024-02-23 12:37:43 -0500

    remove deprecated --empty flag (#38)

--- setup/06-regional-tables/movr/cockroach/run.sh
+++ setup/06-regional-tables/movr/cockroach/run.sh
@@ -4,4 +4,4 @@ IFS=$'\n\t'
 
 
 
-cockroach demo --sql-port 26257 --no-example-database --nodes=9 --demo-locality=region=us-east,zone=a:region=us-east,zone=b:region=us-east,zone=c:region=us-west,zone=b:region=us-west,zone=c:region=us-west,zone=d:region=us-central,zone=a:region=us-central,zone=d:region=us-central,zone=f --empty
+cockroach demo --sql-port 26257 --no-example-database --nodes=9 --demo-locality=region=us-east,zone=a:region=us-east,zone=b:region=us-east,zone=c:region=us-west,zone=b:region=us-west,zone=c:region=us-west,zone=d:region=us-central,zone=a:region=us-central,zone=d:region=us-central,zone=f 
